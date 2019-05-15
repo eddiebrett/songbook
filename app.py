5df58c146948d76402f8134667ab7994c19dec6e
eddie brett <eddiebrett@hotmail.co.uk>
2019-05-15 14:21:38 +0000

    updated names

--- app.py
+++ app.py
@@ -10,30 +10,55 @@ app.config["MONGO_URI"] = 'mongodb+srv://cockneypirate:r00tUser@myfirstcluster-g
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/songs')
-def get_songs():
-    return render_template("songs.html", categories=mongo.db.categories.find())
+@app.route('/song_list')
+def song_list():
+    return render_template("songs.html", songs=mongo.db.songs.find())
     
 @app.route('/add_song')
 def add_song():
-    return render_template("add_song.html", categories=mongo.db.categories.find())
+    return render_template("add_song.html", songs=mongo.db.songs.find())
 
 
 @app.route('/insert_song', methods=['POST'])
 def insert_song():
-    insert = mongo.db.categories
+    insert = mongo.db.songs
     insert.insert_one(request.form.to_dict())
-    return redirect(url_for('get_songs'))
+    return redirect(url_for('song_list'))
     
     
                            
-@app.route('/view_song/<categories_id>')
-def view_song(categories_id):
-    the_task =  mongo.db.categories.find_one({"_id": ObjectId(categories_id)})
-    all_categories =  mongo.db.categories.find()
-    return render_template('view_song.html', task=the_task,
-                           categories=all_categories)
+@app.route('/view_song/<song_id>')
+def view_song(song_id):
+    select_song =  mongo.db.songs.find_one({"_id": ObjectId(song_id)})
+    all_songs =  mongo.db.songs.find()
+    return render_template('view_song.html', select=select_song,
+                           songs=all_songs)
+                           
+@app.route('/edit_song/<song_id>')
+def edit_song(song_id):
+    select_song =  mongo.db.songs.find_one({"_id": ObjectId(song_id)})
+    all_songs =  mongo.db.songs.find()
+    return render_template('edit_song.html', select=select_song,
+                           songs=all_songs)
 
+@app.route('/update_song/<song_id>', methods=["POST"])
+def update_song(song_id):
+    song = mongo.db.songs
+    song.update({'_id': ObjectId(song_id)},
+    {
+        'song_name':request.form.get('song_name'),
+        'song_writer':request.form.get('song_writer'),
+        'song_lyrics': request.form.get('song_lyrics'),
+        'artist_name': request.form.get('artist_name'),
+        'original_song':request.form.get('original_song'),
+        'explicit_lyrics':request.form.get('explicit_lyrics')
+    })
+    return redirect(url_for('songs_list'))
+    
+@app.route('/delete_song/<song_id>')
+def delete_song(song_id):
+    mongo.db.songs.remove({'_id': ObjectId(song_id)})
+    return redirect(url_for('song_list'))
 
 
 if __name__ == '__main__':
